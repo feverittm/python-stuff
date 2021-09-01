4bff73ac278669608fe810fdcd097bab807d2fae
Floyd Moore <floyd.moore@gmail.com>
2021-08-31 20:04:09 -0700

    fetch/cache working again

--- bs4_test.py
+++ bs4_test.py
@@ -15,10 +15,11 @@ except ImportError:
     raise
 
 def cache_html(url, path):
-    encoded = url.encode('utf-8')
-    urlhash=hashlib.sha256(encoded).hexdigest()
+    print(f'path ... {path}')
     with open(path, 'wb') as f:
-        f.write(url)
+        print(url)
+        with urllib.request.urlopen(url) as response:
+            f.write(response.read())
 
 def open_html(path):
     with open(path, 'rb') as f:
@@ -51,49 +52,53 @@ def get_entry(address):
     print("\n")
 
 
+def main():
 
-for url in url_list:
-    encoded = url.encode('utf-8')
-    urlhash = hashlib.sha256(url.encode('utf-8')).hexdigest() + ".htm"
+    for url in url_list:
+        encoded = url.encode('utf-8')
+        urlhash = hashlib.sha256(url.encode('utf-8')).hexdigest() + ".htm"
 
-    if not os.path.exists(urlhash):
-        print(f"Loading missing file {urlhash}")
-        save_html(url, urlhash)
-    page = str(open_html(urlhash))
+        if not os.path.exists(urlhash):
+            print(f"Loading missing file {url}")
+            cache_html(url, urlhash)
+        page = str(open_html(urlhash))
 
-    for i in range(3):
-        tag="</TABLE>"
-        endOfHeader = page.find(tag)
-        index = endOfHeader
-        print(f'{index}\n\n')
-        ch = page[index+len(tag)+3:]
-        page = ch
-        print (page[:10])
+        for i in range(3):
+            tag="</TABLE>"
+            endOfHeader = page.find(tag)
+            index = endOfHeader
+            print(f'{index}\n\n')
+            ch = page[index+len(tag)+3:]
+            page = ch
+            print (page[:10])
 
-    for i in range(2):
-        tag="<BR>"
-        endOfHeader = page.find(tag)
-        index = endOfHeader
-        print(f'{index}\n\n')
-        ch = page[index+len(tag)+2:]
-        page = ch
-        print (page[:10])
+        for i in range(2):
+            tag="<BR>"
+            endOfHeader = page.find(tag)
+            index = endOfHeader
+            print(f'{index}\n\n')
+            ch = page[index+len(tag)+2:]
+            page = ch
+            print (page[:10])
 
-    count=0
-    soup = BeautifulSoup(page, 'html.parser')
-    addresses = soup.find_all('a')
-    for address in addresses:
-        print(f'Count: {count}')
-        get_entry(address)
-        count = count + 1
+        count=0
+        soup = BeautifulSoup(page, 'html.parser')
+        addresses = soup.find_all('a')
+        for address in addresses:
+            print(f'Count: {count}')
+            get_entry(address)
+            count = count + 1
 
-    # state machine for capturing story tags:
-    # 1 - scan from beginning until the end of the second table (2nd /TABLE tag)
-    # 2 - search for second <BR> tag
-    # 3 - Loop around story tags... state machine:
-    #     stories are delimited by blank lines...
-    #       get <A> link tags to get the story link and title
-    #       get text for the description, combine text from multiple lines until end 
-    #           of story is found (double <BR>)
-    #     end story link with <BR><BR>
-    # 4 - story stops with multiple blank lines (>3)
+        # state machine for capturing story tags:
+        # 1 - scan from beginning until the end of the second table (2nd /TABLE tag)
+        # 2 - search for second <BR> tag
+        # 3 - Loop around story tags... state machine:
+        #     stories are delimited by blank lines...
+        #       get <A> link tags to get the story link and title
+        #       get text for the description, combine text from multiple lines until end 
+        #           of story is found (double <BR>)
+        #     end story link with <BR><BR>
+        # 4 - story stops with multiple blank lines (>3)
+
+if __name__ == "__main__":
+    main()
