296169c06239e2451279f4cf6de2739d579e9f25
Floyd E Moore <floyd.moore@hp.com>
2024-08-13 11:16:45 -0700

    last chance

--- sqlite_test.py
+++ sqlite_test.py
@@ -1,27 +1,16 @@
 import sqlite3 as sl
+import pandas as pd
 
-con = sl.connect('my-test.db')
+con = sl.connect(":memory:")
+cur = con.cursor()
 
-with con:
-    con.execute("""
-        CREATE TABLE USER (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            age INTEGER
-        );
-    """)
+df = pd.read_csv("2023_Team_Roster.csv")
+df.to_sql("TEAM", con, if_exists='replace', index=True)
 
-sql = 'INSERT INTO USER (id, name, age) values(?, ?, ?)'
-data = [
-    (1, 'Alice', 21),
-    (2, 'Bob', 22),
-    (3, 'Chris', 23)
-]
+#with con:
+#    con.executemany(sql, data)
 
 with con:
-    con.executemany(sql, data)
-
-with con:
-    data = con.execute("SELECT * FROM USER WHERE age <= 22")
+    data = con.execute("SELECT * FROM TEAM WHERE ACTIVE=1")
     for row in data:
         print(row)
